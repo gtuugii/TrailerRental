f2686b9a8c44a08b6b48af7b03d88a1740585a4b
gtuugii <tuugii83@gmail.com>
2019-06-10 11:38:41 -0500

    create TrailerRental Client

---
+++ TrailerRentalClient/src/test/java/mum/edu/swe/trailerrentalclient/trailerrentalclient/TrailerRentalClientApplicationTests.java
@@ -0,0 +1,16 @@
+package mum.edu.swe.trailerrentalclient.trailerrentalclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrailerRentalClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
