12d3f7ea0d510aeed18932a2947e0f30dcb44cfe
gtuugii <tuugii83@gmail.com>
2019-06-17 20:34:56 -0500

    Create HomeController.java

---
+++ TrailerRentalClient/src/main/java/mum/edu/swe/trailerrentalclient/controller/HomeController.java
@@ -0,0 +1,14 @@
+package mum.edu.swe.trailerrentalclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value={"/", "/home"})
+    public String home(){
+        return "index";
+    }
+
+}
