31eeeb6b0e97f692b6be0c75fada130313c47450
gtuugii <tuugii83@gmail.com>
2019-06-18 23:14:36 -0500

    Update TrailerRepository.java

--- TrailerRentalServer/src/main/java/mum/edu/swe/trailerrentalserver/repository/TrailerRepository.java
+++ TrailerRentalServer/src/main/java/mum/edu/swe/trailerrentalserver/repository/TrailerRepository.java
@@ -2,6 +2,7 @@ package mum.edu.swe.trailerrentalserver.repository;
 
 import mum.edu.swe.trailerrentalserver.domain.Trailer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,17 @@ import java.util.List;
 @Repository
 public interface TrailerRepository extends JpaRepository<Trailer, Long> {
 
-    Trailer findAllByNumber(String number);
+    List<Trailer> findByNumberContainsAndStatus(String number, Integer status);
+    List<Trailer> findAllByNumberContains(String number);
 
+    //  put(0, "MAINTENANCE");
+    //  put(1, "ACTIVE");
+    //  put(2, "PENDING");
+    //  put(3, "RENTED");
+    List<Trailer> findAllByStatus(Integer status);
+
+    @Query(value="UPDATE trailer SET status=:status WHERE trailer_id=:trailer_id", nativeQuery = true)
+    Trailer updateTrailerStatus(Long trailer_id, Integer status);
+
+    Integer countAllByStatus(Integer status);
 }
